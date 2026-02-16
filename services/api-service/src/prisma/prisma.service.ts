55f119ede2ad17a7eedc4a06c9ab9a517b92790c
Jannis Visser <jannisvisser@redcross.nl>
2026-02-16 12:23:24 +0100

    define client differently

--- services/api-service/src/prisma/prisma.service.ts
+++ services/api-service/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
-import { PrismaClient } from '@api-service/src/generated/prisma/client.js';
 import { DATABASE_URL } from '@api-service/src/prisma/prisma.config';
 
 @Injectable()
