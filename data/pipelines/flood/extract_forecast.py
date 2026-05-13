ad9ebd7f749d6d1b90d4ccdf18a8b6083dee4c5d
pphung <45565029+p-phung@users.noreply.github.com>
2026-05-13 16:04:54 +0200

    rm multiplier 100 of mock trigger scenario

--- data/pipelines/flood/extract_forecast.py
+++ data/pipelines/flood/extract_forecast.py
@@ -54,7 +54,7 @@ def extract_discharge_glofas_station(
 
         if forecast_base_datetime is None:
             forecast_base_datetime = _extract_forecast_base_datetime(netcdf_path)
-            for lead_time in range(lead_time_min, lead_time_max+1):
+            for lead_time in range(lead_time_min, lead_time_max + 1):
                 time_interval_start, time_interval_end = _lead_time_to_time_interval(
                     forecast_base_datetime,
                     lead_time,
@@ -70,26 +70,35 @@ def extract_discharge_glofas_station(
         logging.info(f"Extracting station discharge from {netcdf_path}")
         with rasterio.open(netcdf_path) as src:
             station_coords = [(float(station.lon), float(station.lat))]
-            for lead_time in range(lead_time_min, lead_time_max+1):
-                discharge_sampled = list(src.sample(station_coords, indexes=lead_time + 1))
-                discharge_value = float(discharge_sampled[0][0]) * 100 #TODO: *100 for mocking alert. To remove in op code 
-                discharges[station_code][lead_time].ensemble_discharges.append(discharge_value)
+            for lead_time in range(lead_time_min, lead_time_max + 1):
+                discharge_sampled = list(
+                    src.sample(station_coords, indexes=lead_time + 1)
+                )
+                discharge_value = float(
+                    discharge_sampled[0][0]
+                )  # For mocking alert, multiply 100 by adding *100 to this line. For sustainability, instead alert/no-alert will be triggered via differing mock source files TODO AB#41516:
+                discharges[station_code][lead_time].ensemble_discharges.append(
+                    discharge_value
+                )
 
     return discharges
 
 
 def _extract_forecast_base_datetime(netcdf_path: str) -> datetime:
-    """Extract forecast run datetime from a file name like dis_00_2026040800_sliced.nc.""" #TODO: extract date 0 from nc dims instead
+    """Extract forecast run datetime from a file name like dis_00_2026040800_sliced.nc."""  # TODO: extract date 0 from nc dims instead
     basename = os.path.basename(netcdf_path)
     match = re.search(r"_(\d{10})_sliced\.nc$", basename)
     if match is None:
-        raise ValueError(f"Unable to extract forecast date from NetCDF path: {netcdf_path}")
+        raise ValueError(
+            f"Unable to extract forecast date from NetCDF path: {netcdf_path}"
+        )
     return datetime.strptime(match.group(1), "%Y%m%d%H")
 
 
-def _lead_time_to_time_interval(base_datetime: datetime, lead_time_days: int) -> tuple[str, str]:
+def _lead_time_to_time_interval(
+    base_datetime: datetime, lead_time_days: int
+) -> tuple[str, str]:
     target_date = base_datetime + timedelta(days=lead_time_days)
     time_interval_start = target_date.strftime("%Y-%m-%dT00:00:00Z")
     time_interval_end = target_date.strftime("%Y-%m-%dT23:59:59Z")
     return time_interval_start, time_interval_end
-
