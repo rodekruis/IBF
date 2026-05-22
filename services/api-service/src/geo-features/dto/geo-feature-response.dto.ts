5a95e5b4a4844ec56e88cd0a6696f2e85ff97a31
Jannis Visser <jannisvisser@redcross.nl>
2026-05-22 10:40:36 +0200

    knip

--- services/api-service/src/geo-features/dto/geo-feature-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IntersectionType } from '@nestjs/swagger';
-
-import { GeoFeatureCreateDto } from '@api-service/src/geo-features/dto/geo-feature-create.dto';
-import { BaseDto } from '@api-service/src/shared/dto/base.dto';
-
-export class GeoFeatureResponseDto extends IntersectionType(
-  BaseDto,
-  GeoFeatureCreateDto,
-) {}
