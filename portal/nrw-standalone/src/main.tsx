d9ab0814a42dc527f096f9807f2b4bd4e220ef26
e3r3i3k3 <ehill@redcross.nl>
2026-06-12 11:47:44 +0200

    add changes 2

--- portal/nrw-standalone/src/main.tsx
+++ portal/nrw-standalone/src/main.tsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import NrwMapContainer from '#components/NrwMap';
+import NrwPage from './components/NrwPage';
 import '@ifrc-go/ui/index.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NrwMapContainer />} />
-        <Route path="/nrw" element={<NrwMapContainer />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <NrwPage>
+        <Routes>
+          <Route path="/" element={<NrwMapContainer />} />
+          <Route path="/nrw" element={<NrwMapContainer />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </NrwPage>
     </BrowserRouter>
   </StrictMode>,
 )
