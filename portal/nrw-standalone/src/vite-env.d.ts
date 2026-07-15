0783ac9288ca1ca877022b728da126e821fcc37a
jannisvisser <jannisvisser@redcross.nl>
2026-07-15 10:40:25 +0200

    chore: use mapbox access token (#312)

--- portal/nrw-standalone/src/vite-env.d.ts
+++ portal/nrw-standalone/src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly APP_MAPTILER_API_KEY: string;
-  readonly APP_IBF_API_BACKEND: string;
-  readonly APP_SEED_DATA_REPO: string;
-  readonly APP_NRW_PORTAL_MODE: string;
+    readonly APP_MAPBOX_ACCESS_TOKEN: string;
+    readonly APP_IBF_API_BACKEND: string;
+    readonly APP_SEED_DATA_REPO: string;
+    readonly APP_NRW_PORTAL_MODE: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv;
 }
