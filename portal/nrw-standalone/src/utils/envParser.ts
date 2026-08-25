d8d80987780fc47b24dfd16224a70382f564cd27
e3r3i3k3 <ehill@redcross.nl>
2026-08-25 15:08:05 +0200

    APP_IBF_API_BACKEND to APP_NRW_API_BACKEND

--- portal/nrw-standalone/src/utils/envParser.ts
+++ portal/nrw-standalone/src/utils/envParser.ts
@@ -2,7 +2,7 @@
 // These are imported as `#config`
 const env = import.meta.env;
 
-export const ibfApiBackend: string = env.APP_IBF_API_BACKEND ?? '';
+export const nrwApiBackend: string = env.APP_NRW_API_BACKEND ?? '';
 export const mbtoken: string = env.APP_MAPBOX_ACCESS_TOKEN ?? '';
 export const seedDataRepo: string = env.APP_SEED_DATA_REPO ?? '';
 export const nrwPortalMode: string = env.APP_NRW_PORTAL_MODE ?? '';
