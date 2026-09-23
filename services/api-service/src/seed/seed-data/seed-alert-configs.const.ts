e60d066c94a8a50d3658a86d743f332ab69554f5
Jannis Visser <jannisvisser@redcross.nl>
2026-09-23 12:58:59 +0200

    real spatial extents

--- services/api-service/src/seed/seed-data/seed-alert-configs.const.ts
+++ services/api-service/src/seed/seed-data/seed-alert-configs.const.ts
@@ -542,9 +542,6 @@ export const SEED_TROPICAL_CYCLONE_ALERT_CONFIGS: SeedAlertConfig[] =
 
 // --- COMPOUND FLOODS: in-code config ---
 // TODO: should some of the below come from a seed-data repo source instead of defined in code?
-// TODO: PHL deepestAdminLevel is currently 3 in seed-countries.const.ts; admin-4 pcodes require
-// updating the admin-area seed pipeline for PHL and bumping deepestAdminLevel.
-// TODO: replace placeholder area names and pcodes with the final values.
 
 const COMPOUND_FLOODS_LEAD_TIME_SPECTRUM = [
   '0-day',
@@ -585,9 +582,77 @@ interface CompoundFloodsArea {
 
 const COMPOUND_FLOODS_AREAS_BY_COUNTRY: Record<string, CompoundFloodsArea[]> = {
   PHL: [
-    { spatialExtentName: 'Area 1', spatialExtentPlaceCodes: [] },
-    { spatialExtentName: 'Area 2', spatialExtentPlaceCodes: [] },
-    { spatialExtentName: 'Area 3', spatialExtentPlaceCodes: [] },
+    {
+      spatialExtentName: 'Pasig River',
+      spatialExtentPlaceCodes: [
+        'PH1303901',
+        'PH1307401',
+        'PH1307402',
+        'PH1307403',
+        'PH1307404',
+        'PH1307405',
+        'PH1307501',
+        'PH1307502',
+        'PH1307503',
+        'PH1307504',
+        'PH1307601',
+        'PH1307602',
+        'PH1307604',
+        'PH1307605',
+        'PH1307606',
+        'PH1307607',
+      ],
+    },
+    {
+      spatialExtentName: 'Santa Maria River',
+      spatialExtentPlaceCodes: [
+        'PH0301402',
+        'PH0301404',
+        'PH0301405',
+        'PH0301408',
+        'PH0301410',
+        'PH0301411',
+        'PH0301412',
+        'PH0301414',
+        'PH0301415',
+        'PH0301417',
+        'PH0301423',
+      ],
+    },
+    {
+      spatialExtentName: 'Bicol River',
+      spatialExtentPlaceCodes: [
+        'PH0501704',
+        'PH0501707',
+        'PH0501708',
+        'PH0501709',
+        'PH0501710',
+        'PH0501713',
+        'PH0501718',
+        'PH0501720',
+        'PH0501721',
+        'PH0501722',
+        'PH0501724',
+        'PH0501726',
+        'PH0501728',
+        'PH0501732',
+        'PH0501734',
+        'PH0501737',
+      ],
+    },
+    {
+      spatialExtentName: 'Panay River',
+      spatialExtentPlaceCodes: [
+        'PH0601905',
+        'PH0601907',
+        'PH0601909',
+        'PH0601910',
+        'PH0601911',
+        'PH0601912',
+        'PH0601913',
+        'PH0601914',
+      ],
+    },
   ],
 };
 
